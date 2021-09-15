07e9d3df2f87c865e124d40ea1e55bcf3f6927a2
Nancy <nancyweibe@gmail.com>
2021-09-15 14:07:42 +0300

    Update: Optimization P2

--- components/ScrollbarWrapper/index.jsx
+++ components/ScrollbarWrapper/index.jsx
@@ -1,9 +1,18 @@
+import { useEffect, useState } from 'react'
 import styles from './scrollbar.module.scss'
 import Scrollbar from "react-smooth-scrollbar"
 
 const ScrollBarWrapper = ({children, className, onInit, alwaysShowTracks}) => {
 
-  return <Scrollbar alwaysShowTracks={alwaysShowTracks} ref={onInit} className={`${styles.scrollBarWrapper} ${className ? className : ""} scoll-bar`}>
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(()=>{
+    if(window.innerWidth < 990) {
+      setIsMobile(true)
+    }
+  }, [])
+
+  return isMobile ? children : <Scrollbar alwaysShowTracks={alwaysShowTracks} ref={onInit} className={`${styles.scrollBarWrapper} ${className ? className : ""} scoll-bar`}>
       {children}
     </Scrollbar>
 }
